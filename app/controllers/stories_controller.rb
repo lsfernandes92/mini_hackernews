32beaaa273d13381ff4825cfb72b91fdfc48b87c
lsfernandes92 <lsfernandes92@gmail.com>
2019-06-13 22:27:22 -0300

    Add submit and delete story

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,40 @@
 class StoriesController < ApplicationController
+  before_action :set_produto, only: [:destroy]
+
   def index
     @stories = Story.all
   end
+
+  def new
+    @story = Story.new
+    render :new
+  end
+
+  def create
+    @story = Story.new story_params
+
+    if @story.save
+      flash[:notice] = "Story submitted successfully"
+      redirect_to root_url
+    else
+      flash.now[:notice] = "Fail to submit story"
+      render :new
+    end
+  end
+
+  def destroy
+    @story.destroy
+    redirect_to root_url
+  end
+
+    private
+
+    def story_params
+      params.require(:story).permit(:title, :url)
+    end
+
+    def set_produto
+      id = params[:id]
+      @story = Story.find(id)
+    end
 end
