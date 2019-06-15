da8963288d7c0136fc5df73558ee814b486d6a34
lsfernandes92 <lsfernandes92@gmail.com>
2019-06-15 20:11:37 -0300

    Add validations and helper methods to Models classes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,12 @@
 class Comment < ApplicationRecord
+  belongs_to :commentable, polymorphic: true
+  has_many :comments, as: :commentable
+
   validates :body, presence: true
+
+  def root_story
+    story = self
+    story = story.commentable until story.commentable.is_a?(Story)
+    story.commentable
+  end
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,22 @@
 class Story < ApplicationRecord
+  has_many :comments, as: :commentable, dependent: :destroy
+
   validates :title, presence: true, length: { maximum: 110 }
   validates :url, presence: true
+
+  def url_host
+    URI("#{self.url}").host || "unknown"
+  end
+
+  def all_comments
+    parent_comments + child_comments
+  end
+
+  def parent_comments
+    Comment.where(commentable_id: id)
+  end
+
+  def child_comments
+    Comment.where(commentable_id: parent_comments.map(&:id))
+  end
 end
